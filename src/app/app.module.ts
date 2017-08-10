d997d5bd9a9e58df825362ba5968edebf481f285
anouar99pac <Anouar>
2017-08-11 01:01:27 +0200

    refs # Module CKEditorModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { CKEditorModule } from 'ng2-ckeditor';
+import { CkeditorComponent } from './ckeditor/ckeditor.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CkeditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
